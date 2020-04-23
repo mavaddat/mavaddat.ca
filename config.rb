77ec4273bf92d728d262f060b26a04d8f25efea9
Mavaddat Javid <mavaddat.javid@gmail.com>
2020-04-23 15:41:09 -0700

    Small update to keybase verification

---
+++ config.rb
@@ -0,0 +1,23 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "C:/Users/MavaddatJavid/Documents/GitHub/mavaddat.ca/html"
+css_dir = "sass"
+sass_dir = "sass"
+images_dir = "images"
+generated_images_path = "C:/Users/MavaddatJavid/Documents/GitHub/mavaddat.ca/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
